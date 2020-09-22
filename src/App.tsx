09973e2d6f3fff8333430f9a3accada5f54743bf
adbarc92 <adbarclay92@gmail.com>
2020-09-21 19:58:49 -0600

    Adds Konva dependency; adds Konva-canvas and background gradient to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import DrawableCanvas from './components/DrawableCanvas';
 import Header from './components/Header';
+import ReactCanvas from './components/ReactCanvas';
 
 const App = (): JSX.Element => {
   // let imageLoaded = Canvas();
   return (
     <div>
       <Header />
-      <DrawableCanvas />
+      {/* <DrawableCanvas /> */}
+
+      <ReactCanvas />
     </div>
   );
 };
